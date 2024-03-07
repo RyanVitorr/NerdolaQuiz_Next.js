1b39929754de780c0ab10b60eed7c7d983e90600
Ryan  Vitor <127523720+RyanVitorr@users.noreply.github.com>
2024-03-07 17:10:06 -0300

    mudanças nos componentes react

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-"use client"
+"use client";
+import React from 'react';
 import * as C from './pageStyle';
 import { Card } from '../_components/Card/index';
 import Link from 'next/link';
--- src/pages/Resultado/[acertos]-[total].tsx
+++ src/pages/Resultado/[acertos]-[total].tsx
@@ -1,10 +1,11 @@
 "use client";
+import React from 'react';
 import '../../app/globals.css';
 import * as C from './pageStyle';
 import { useRouter } from 'next/router';
 import { Card } from '../../_components/Card/index';
 
-export default function Resultado () {
+export default function Resultado()  {
     const router = useRouter();
     const { acertos, totalQuestions } = router.query;
 
